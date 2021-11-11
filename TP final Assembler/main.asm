ba5e6deac1cff3505e9e0e10e6cdaf66da364d61
joaquinvoeffray <joaquinv1234@hotmail.com>
2021-11-11 20:45:18 -0300

    datos en la eeprrom

--- TP final Assembler/main.asm
+++ TP final Assembler/main.asm
@@ -1,9 +1,19 @@
+
+.dseg
+R: .db 'R'			//agregado de bariables en segmeto de datos
+A: .db 'A'			//se agrega el'a' para definir que es un bit	
+r: .db 'R'
+a: .db 'A'				
+
+.cseg
 .org 0x0000
 jmp inicio
 .org 0x0004
 jmp isr_I1
 
 .include "Division.inc"
+
+
 
 inicio:
 	ldi r16,high(RamEnd)  
@@ -31,21 +41,39 @@ inicio:
 	Sts Eicra,r16
 	Ldi r16,(1<<Int1)
 	out Eimsk,r16
+ 
+ //guardamos los valores de las variables definidas
 
+	ldi	r16,65
+	sts A,r16
+
+	ldi	r16,97
+	sts a,r16
+
+	ldi r16,82
+	sts R,r16
+
+	ldi r16,114
+	sts r,r16
 
 	sei
 
 
 	volver1:
+
+
+
+
 	Rjmp volver1
 							
 
-Isr_I1:
+Isr_I1:										
 	inc r17
 	cpi r17,100
 	breq Cuenta1Seg
 	rjmp volver
-	Cuenta1Seg:
+
+	Cuenta1Seg:								//hacer la cuenta fuera del tratamiento de interrupciones 
 	inc r16
 	MOV	R24,R16
 
@@ -66,17 +94,14 @@ Isr_I1:
 	ENVIO_UART:
 		PUSH	R19
 		STS		UDR0,R20
-ESPERAR_TX:		
+	ESPERAR_TX:		
 		LDS		R19, UCSR0A
 		SBRS	R19,UDRE0
 		RJMP	ESPERAR_TX
 		POP		R19
 		
-
-
-	
-	volver:
-	Reti
+	Volver:
+		Reti
 
 
 	
